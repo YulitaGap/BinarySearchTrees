d06e28f64e2cfc81d9a8434526fec621109b256d
Yulia Gapunovych <45093751+YulitaGap@users.noreply.github.com>
2019-05-26 00:45:35 +0300

    Update game.py

--- xo_task3/game.py
+++ xo_task3/game.py
@@ -2,58 +2,71 @@ from board import Board
 from btree import Tree
 
 cells = {1: (0, 0), 2: (0, 1), 3: (0, 2), 4: (1, 0),
-         5: (1, 1), 6: (1, 2), 7: (2, 0), 8: (2, 1), 9: (2, 2)}
+         5: (1, 1), 6: (1, 2), 7: (2, 0), 8: (2, 1),
+         9: (2, 2)}
 
-class Game:
+
+class GameLogic:
+
     def __init__(self):
-        sign = input('> If you want to play for a X - press X, else print 0 :')
+        """
+        Initialization of TicTocToe GameLogic class.
+        """
         print('___________________________________________________________')
         print('WELCOME TO THE TIC.TOC.TOE GAME!')
         print('THIS IS A STARTING EMPTY BOARD!')
-        if sign == 'X' or sign == 'x':
-            self.player = 1
-            self.board = Board(True)
-        elif sign == 'O' or sign == '0':
-            self.player = -1
-            self.board = Board(False)
+        self.board = None
+        while not self.board:
+            sign = input('> If you want to play for a X - press X, else print 0 :')
+            if sign == 'X' or sign == 'x':
+                self.player = 1
+                self.board = Board(True)
+            elif sign == 'O' or sign == '0' or sign == 'o':
+                self.player = -1
+                self.board = Board(False)
         self.tree = Tree(self.board)
 
     def game(self):
         self.tree.root.board.draw()
-        if self.player == 1:
-            print('YOUR TURN!')
-            move_cell = int(input('>>> Enter a number of cell from 1 to 9 : '))
-            cell = cells[move_cell]
-            while not self.tree.root.board.check_user_move(cell):
-                move_cell = int(input('>>> Enter a number of cell from 1 to 9 : '))
-                cell = cells[move_cell]
-            self.tree = self.tree.move(cell)
-        else:
-            print('Computer is choosing his move...')
-            cell = self.tree.search_best_move()
-            self.tree = self.tree.move(cell)
+        self.play = True
+        while self.play:
+            if self.player == 1:
+                print('YOUR TURN!')
+                try:
+                    move_cell = int(input('>>> Enter a number of cell from 1 to 9 : '))
+                    cell = cells[move_cell]
+                except KeyError:
+                    move_cell = int(input('>>> Enter a number of cell from 1 to 9 : '))
+                    cell = cells[move_cell]
+                while not self.tree.root.board.check_user_move(cell):
+                    move_cell = int(input('>>> Enter a number of cell from 1 to 9 : '))
+                    cell = cells[move_cell]
+                self.tree = self.tree.move(cell)
+            else:
+                print("BOT's TURN!")
+                cell = self.tree.choose_optimal_way()
+                self.tree = self.tree.move(cell)
 
-        current_state = self.tree.root.board.has_winner()
-        if current_state == 2:  # no winner
-            self.tree.root.board.draw()
-            print('That`s a draw')
-            return False
-        elif current_state == 1:
-            self.tree.root.board.draw()
-            print('You loose')
-            return False
-        elif current_state == -1:
-            self.tree.root.board.draw()
-            print('You win')
-            return False
-        self.player = -self.player
-        return True
+            current_state = self.tree.root.board.has_winner()
+            if current_state == 2:  # no winner
+                self.tree.root.board.draw()
+                self.play = False
 
-    def play(self):
-        fl = True
-        while fl:
-            fl = self.game()
+            elif current_state == -1:
+                self.tree.root.board.draw()
+                print('YOU WIN!')
+                self.play = False
+                break
+
+            elif current_state == 1:
+                self.tree.root.board.draw()
+                print('COMPUTER WIN!')
+                self.play = False
+                break
+
+            self.player = -self.player
+            self.play = True
 
 
 if __name__ == '__main__':
-    Game().play()
+    GameLogic().game()
