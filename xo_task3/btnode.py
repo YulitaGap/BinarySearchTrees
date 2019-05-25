d9766aaebb683cb3a74d03cb5a2999aade124a0c
Yulia Gapunovych <45093751+YulitaGap@users.noreply.github.com>
2019-05-25 14:02:30 +0300

    Update btnode.py

--- xo_task3/btnode.py
+++ xo_task3/btnode.py
@@ -1,12 +1,14 @@
 class Node:
-    """Class for Node representation"""
-    def __init__(self, value, left=None, right=None):
-        """
-        Initialization of Node
-        """
-        self.left = left
-        self.right = right
-        self.value = value
+    """
+    Class for Tree Node representation
+    """
+    def __init__(self, board, cell=None, result=0):
+        self.board = board
+        self.cell = cell
+        self.result = result
 
-    def get_value(self):
-        return self.value
+    def move(self, move_cell):
+        return self.board.move(move_cell)
+
+    def check_state(self):
+        return self.board.has_winner() 
